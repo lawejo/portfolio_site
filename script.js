06f2ad2aa76966ec903080937874d815f1eb9f10
Lasse Weber Joergensen <lass876q@stud.kea.dk>
2022-04-04 12:52:10 +0200

    minor updates

--- script.js
+++ script.js
@@ -2,8 +2,18 @@
 
 /* ---------------Nav / Burgermenu----------------------- */
 document.querySelector("nav svg").addEventListener("click", slideOutMenu);
+document.querySelector(".burgermenu li").addEventListener("click", closemenu);
+const burgermenuClass = document.querySelector(".bgmenuWidth");
+const burgermenu = document.querySelector(".burgermenu");
+
+function closemenu() {
+  if (!burgermenu.classList.contains(burgermenuClass)) {
+    slideOutMenu();
+  }
+}
 function slideOutMenu() {
   document.querySelector(".burgermenu").classList.toggle("bgmenuWidth");
+  document.querySelector("nav svg").classList.toggle("nav_svg_fixed");
 }
 const mediaSize = window.matchMedia("(max-width: 556px)");
 if (mediaSize.matches) {
